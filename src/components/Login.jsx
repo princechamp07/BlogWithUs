59a1919e72632519db81dd7752b26e78a0793204
Prince <princechamp12906@gmail.com>
2024-05-09 19:45:47 +0530

    Posts load

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -88,7 +88,7 @@ function Login(props) {
                        Sign In With Google
                    </Button>
                    <Button className="flex mt-4 bg-blue-700 justify-center  text-center items-center h-10 w-full">
-                       <Link to="/Posts">
+                       <Link to="BlogWithUs/Posts">
                            Check Posts as guest
                        </Link>
                    </Button>
